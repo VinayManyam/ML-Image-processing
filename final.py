fb2f67974dd29469d8c07c1c4c44e3853865bbaa
vinay manyam <vinayip541@gmail.com>
2021-09-16 20:32:07 +0530

    Update final.py

--- final.py
+++ final.py
@@ -3,5 +3,5 @@ from detecto import core, utils, visualize
 def check(image1):
 	 image = utils.read_image(image1)
 	 model = core.Model()
-	 labels, boxes, scores = model.predict_top(image)
+	 labels, boxes, scores = model.predict(image)
 	 visualize.show_labeled_image(image, boxes, labels)
