de3edcd31cf46ab24af7c9af1c5a7b0da83af687
vinay manyam <vinayip541@gmail.com>
2022-06-18 11:01:11 +0530

    Update cuda_tamplate_match.py
    opencv cuda template matching with python

--- Template Matching/cuda_tamplate_match.py
+++ Template Matching/cuda_tamplate_match.py
@@ -10,16 +10,15 @@ gtmpl = cv2.cuda_GpuMat()
 gresult = cv2.cuda_GpuMat()
 gsrc.upload(src)
 gtmpl.upload(needle)
-match_time = time.time()
 matcher = cv2.cuda.createTemplateMatching(cv2.CV_8UC1, cv2.TM_CCOEFF_NORMED)
 gresult = matcher.match(gsrc, gtmpl)
-result_time = time.time()
 resultg = gresult.download()
 w, h = needle.shape[::-1]
 loc = np.where( resultg >= threshold)
+
 if not loc[1].size==0:
     for pt in zip(*loc[::-1]):
         cv2.rectangle(src, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-        print("Match Found :")
-        cv2.imwrite("final1.jpg",src)
+        print("Match Found")
+        cv2.imwrite("final.jpg",src)
         break   
